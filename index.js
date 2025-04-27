557ba76ac6c72fc866a053cefbcd2b4c715ee2c8
Paschal Enyimiri <paschal.enyimiri@indicina.co>
2025-04-27 20:29:07 +0100

    removed internal exports

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-// Export all exports from your helper files
-export * from "./lib/helpers/buildK6Script";
-export * from "./lib/helpers/generateHeaders";
-export * from "./lib/helpers/resolveBody";
-export * from "./lib/utils/k6Runner";
 export * from "./step_definitions/load_test_steps.js";
